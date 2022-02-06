1075e33f1b2392b2a5c943cf8d87bad3e63febd2
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-06 22:03:22 +0900

    closure practice

--- practice.go
+++ practice.go
@@ -2,46 +2,33 @@ package main
 
 import "fmt"
 
-//func add(x int, y int) => (x, y int)に省略できる
-//1個目の（）は引数
-//2個目の（）は返り値
-func add(x, y int) (int, int) {
-	return x + y, x - y
+func incrementGenerator() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
 }
 
-//返す変数名を指定した時
-/**
-*	関数内で、resultを定義はしなくても良い、するとエラーになる
-*	（return　変数名）で返さなくても、良い
-*	何が返り値かをわかりやすくする
- */
-func calc(price, item int) (result int) {
-	result = price * item
-	return
-}
-
-func convert(price int) float64 {
-	return float64(price)
+func circleArea(pi float64) func(radius float64) float64 {
+	return func(radius float64) float64 {
+		return pi * radius * radius
+	}
 }
 
 func main() {
-	r, r2 := add(100, 200)
-	fmt.Println(r)
-	fmt.Println(r2)
+	counter := incrementGenerator()
+	fmt.Println(counter())
+	fmt.Println(counter())
+	fmt.Println(counter())
+	fmt.Println(counter())
 
-	r3 := calc(1000, 20)
-	fmt.Println(r3)
+	c1 := circleArea(3.14)
+	fmt.Println(c1(2))
+	fmt.Println(c1(3))
 
+	c2 := circleArea(3)
+	fmt.Println(c2(2))
+	fmt.Println(c2(3))
 
-	//変数に関数を追加する
-	f := func(x int) {
-		fmt.Println("inner func", x)
-	}
-	f(1)
-
-
-	//並列化で「go」をつけて並列処理を実行することができるので覚えておいて損はない
-	func(x int) {
-		fmt.Println("inner func", x)
-	}(1)
 }
