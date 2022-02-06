84b06817e44fcb630250a90b4ec479b7b0286c95
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-06 18:02:11 +0900

    byte practice

--- practice.go
+++ practice.go
@@ -3,34 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	m := map[string]int{"apple": 100, "banana": 200}
+	b := []byte{72, 73}
+	fmt.Println(b)
+	fmt.Println(string(b))
 
-	fmt.Println(m)
-	fmt.Println(m["apple"])
-	m["new"] = 500
-	fmt.Println(m)
+	c := []byte("OK")
+	fmt.Println(c)
+	fmt.Println(string(c))
 
-	//ない場合は、0が返ってくる
-	fmt.Println(m["nothing"])
-
-	v, ok := m["apple"]
-	fmt.Println(v, ok)
-
-	v2, ok2 := m["nothing"]
-	fmt.Println(v2, ok2)
-
-	m2 := make(map[string]int)
-	m2["pc"] = 5000
-	fmt.Println(m2)
-
-	//メモリ上に入れるmapがないので入れることができない
-	//var m3 map[string]int
-	//m3["pc"] = 5000
-	//fmt.Println(m3)
-
-	//varで宣言した時は、slice,map両方で「Nil」という値が入ってくる
-	var s []int
-	if s == nil {
-		fmt.Println("Nil")
-	}
 }
