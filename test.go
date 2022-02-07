78dfd342168e8ee4313260acc0539f9933d077a1
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-07 19:20:35 +0900

    update

--- test.go
+++ test.go
@@ -6,6 +6,7 @@ package main
 //	"net/http"
 //)
 //
+
 //func main() {
 //	// URLのパターンとハンドラーを渡す
 //	http.HandleFunc("/foo", fooHandlerFunc)
@@ -15,11 +16,38 @@ package main
 //	}
 //}
 //
-//// `/foo` へリクエストがきたときに実行されるハンドラー
-//// http.ResponseWriterにコンテンツを書き込む。
-//// もちろんHTMLテンプレートを使ったりJSONを返すことも可能
+
+// `/foo` へリクエストがきたときに実行されるハンドラー
+// http.ResponseWriterにコンテンツを書き込む。
+// もちろんHTMLテンプレートを使ったりJSONを返すことも可能
 //func fooHandlerFunc(w http.ResponseWriter, r *http.Request) {
 //	fmt.Fprintf(w, "/fooページです！！")
 //	fmt.Println("testtest")
 //	fmt.Fprintf(w, "testest")
+//}
+
+
+//package main
+//
+//import (
+//"fmt"
+//"net/http"
+//)
+
+//func handler(writer http.ResponseWriter, request *http.Request)	{
+//	path := request.URL.Path[1:]
+//
+//	if path == "testest" {
+//		fmt.Fprintf(writer, "Good Morning!! %v!", path)
+//	} else {
+//		fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+//	}
+//
+//}
+//
+//func main() {
+//	http.HandleFunc("/", handler)
+//	http.HandleFunc("/testest", handler)
+//	http.ListenAndServe(":8080", nil)
+//
 //}
