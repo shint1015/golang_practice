72efa94612f1a217ff56c7f1a883f6e1545f9529
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-08 23:37:39 +0900

    panic recover handling practice

--- practice.go
+++ practice.go
@@ -1,27 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-)
+import "fmt"
+
+/*
+panicは、どうすれば良いのかわからなくする要素があるので
+なるべく使わない方が良い
+recover()は、panicをキャッチして処理が終了しないようにすることができる
+*/
+func thirdPartyConnectDB() {
+
+	panic("Unable to connect database!")
+}
+
+func save() {
+	defer func() {
+		s := recover()
+		fmt.Println(s)
+	}()
+	thirdPartyConnectDB()
+}
 
 func main() {
-	file, err := os.Open("./practice.go")
-	if err != nil {
-		log.Fatal("error")
-	}
-	defer file.Close()
-	data := make([]byte, 100)
-	//変数の一個目だけイニシャライズすれば、大丈夫で
-	//変数の2個目は、イニシャライズの対象にはならず、上書きされていく。
-	count, err := file.Read(data)
-	if err != nil {
-		log.Fatalln("Error")
-	}
-	fmt.Println(count, string(data))
-
-	if err = os.Chdir("test"); err != nil {
-		log.Fatalln("err")
-	}
+	save()
+	fmt.Println("ok?")
 }
