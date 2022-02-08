069005f22d9691ae4205a74bd048aa8bff052121
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-08 23:48:15 +0900

    ennsyuu2 practice

--- practice.go
+++ practice.go
@@ -2,25 +2,30 @@ package main
 
 import "fmt"
 
-/*
-panicは、どうすれば良いのかわからなくする要素があるので
-なるべく使わない方が良い
-recover()は、panicをキャッチして処理が終了しないようにすることができる
-*/
-func thirdPartyConnectDB() {
-
-	panic("Unable to connect database!")
-}
-
-func save() {
-	defer func() {
-		s := recover()
-		fmt.Println(s)
-	}()
-	thirdPartyConnectDB()
-}
-
 func main() {
-	save()
-	fmt.Println("ok?")
+	//Q1 : 以下のスライスから一番小さい数を探して出力するコードを書いてください。
+	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
+	min := l[0]
+	for _, v := range l {
+		if min > v {
+			min = v
+		}
+	}
+	fmt.Println(min)
+
+	//Q2 : 以下の果物の価格の合計を出力するコードを書いてください。
+	m := map[string]int{
+		"apple":  200,
+		"banana": 300,
+		"grapes": 150,
+		"orange": 80,
+		"papaya": 500,
+		"kiwi":   90,
+	}
+	sum := 0
+	for _, k := range m {
+		sum += k
+	}
+	fmt.Println(sum)
+
 }
