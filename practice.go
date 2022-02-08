c4be29c1a664db9fec3a9e205e13b630abff7780
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-08 23:28:25 +0900

    error handling practice

--- practice.go
+++ practice.go
@@ -2,31 +2,26 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 )
 
-//基本的なログファイルの設定は以下のような感じで良い
-func LoggingSetting(logFile string) {
-	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	multiLogFile := io.MultiWriter(os.Stdout, logfile)
-	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
-	log.SetOutput(multiLogFile)
-}
-
 func main() {
-	LoggingSetting("test.log")
-	_, err := os.Open("gdemnslkmw")
+	file, err := os.Open("./practice.go")
 	if err != nil {
-		log.Fatalln("exit", err)
+		log.Fatal("error")
 	}
+	defer file.Close()
+	data := make([]byte, 100)
+	//変数の一個目だけイニシャライズすれば、大丈夫で
+	//変数の2個目は、イニシャライズの対象にはならず、上書きされていく。
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatalln("Error")
+	}
+	fmt.Println(count, string(data))
 
-	log.Println("logging")
-	log.Printf("%T %v", "test", "test")
-
-	//処理がそこで終了する
-	log.Fatalf("%T %v", "test", "test")
-	log.Fatalln("error")
-	fmt.Println("ok")
+	if err = os.Chdir("test"); err != nil {
+		log.Fatalln("err")
+	}
 }
