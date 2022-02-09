3ddad2b0b1d330db25a7881708965ecd7272692e
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-09 23:15:55 +0900

    pointa practice

--- practice.go
+++ practice.go
@@ -2,30 +2,19 @@ package main
 
 import "fmt"
 
+//ポインタ
+//アドレス、参照渡し
+func one(x *int) {
+	*x = 1
+}
+
 func main() {
-	//Q1 : 以下のスライスから一番小さい数を探して出力するコードを書いてください。
-	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
-	min := l[0]
-	for _, v := range l {
-		if min > v {
-			min = v
-		}
-	}
-	fmt.Println(min)
-
-	//Q2 : 以下の果物の価格の合計を出力するコードを書いてください。
-	m := map[string]int{
-		"apple":  200,
-		"banana": 300,
-		"grapes": 150,
-		"orange": 80,
-		"papaya": 500,
-		"kiwi":   90,
-	}
-	sum := 0
-	for _, k := range m {
-		sum += k
-	}
-	fmt.Println(sum)
-
+	var n int = 100
+	one(&n)
+	fmt.Println(n)
+	fmt.Println(*&*&n)
+	fmt.Println(&n)
+	var p *int = &n
+	fmt.Println(p)
+	fmt.Println(*p)
 }
