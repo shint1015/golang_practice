07c4219adaa7631e84b7cec8c80fc5ea53bd558e
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-10 23:28:28 +0900

    ensyuu3 practice

--- practice.go
+++ practice.go
@@ -2,59 +2,28 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	//X,Y int　短縮した形でも表わせる
-	X int
-	Y int
-	S string
-}
-
-func changeVertex(v Vertex) {
-	v.X = 1000
-}
-
-func changeVertex2(v *Vertex) {
-	v.X = 1000
-	//本当は以下のように書くのが正しいが、structの場合は勝手に変換してくれる
-	//(*v).X = 1000
-}
-
 func main() {
-	vv := Vertex{1, 2, "test"}
-	changeVertex(vv)
-	fmt.Println(vv)
-
-	vv2 := &Vertex{1, 2, "test"}
-	changeVertex2(vv2)
-	fmt.Println(*vv2)
-	v := Vertex{X: 1, Y: 2}
-	fmt.Println(v)
-	fmt.Println(v.X, v.Y)
-	v.X = 100
-	fmt.Println(v.X, v.Y)
-
-	//yの値は指定していない場合は、0となる
-	v2 := Vertex{X: 1}
-	fmt.Println(v2)
-
-	//値だけの場合は、全部の値を入れないとエラーが起こる
-	v3 := Vertex{1, 2, "test"}
-	// ×　v3 := Vertex{1, 2}
-	fmt.Println(v3)
-
-	//v4, v5は同じ
-	v4 := Vertex{}
-	fmt.Printf("%T %v\n", v4, v4)
-
-	var v5 Vertex
-	fmt.Printf("%T %v\n", v5, v5)
-
-	//newで宣言する時は、v7の宣言と同じ
-	v6 := new(Vertex)
-	fmt.Printf("%T %v\n", v6, v6)
-
-	//こっちの宣言の方が使われるかも
-	v7 := &Vertex{}
-	fmt.Printf("%T %v\n", v7, v7)
-
+	/*
+		var i int = 10
+		var p *int
+		p = &i
+		var j int
+		j = *p
+		fmt.Println(j)
+	*/
+	var i int = 100
+	var j int = 200
+	var p1 *int
+	var p2 *int
+	p1 = &i
+	p2 = &j
+	i = *p1 + *p2
+	//p1には、iのアドレスが入っているから
+	//この時点で*p1=300
+	//ここでp1のアドレスがp2に入る
+	p2 = p1
+	//fmt.Println(*p2)
+	//*p2=300 + i = 300 = 600
+	j = *p2 + i
+	fmt.Println(j)
 }
