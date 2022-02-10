9885d2d8ec9f24af17f2c37735eda488bcbaa4af
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-10 23:57:55 +0900

    create struct pattern practice

--- practice.go
+++ practice.go
@@ -3,29 +3,35 @@ package main
 import "fmt"
 
 type Vertex struct {
-	X, Y int
+	x, y int
 }
 
 func Area(v Vertex) int {
-	return v.X * v.Y
+	return v.x * v.y
 }
 
 // 値レシーバー
 func (v Vertex) Area() int {
-	return v.X * v.Y
+	return v.x * v.y
 }
 
 // ポインタレシーバー
 //　参照して、structの中身に変更を加える
 func (v *Vertex) Scale(i int) {
-	v.X = v.X * i
-	v.Y = v.Y * i
+	v.x = v.x * i
+	v.y = v.y * i
+}
+
+//Golangのパターン
+//newでストラクタを作成することがよくあるパターン
+func New(x, y int) *Vertex {
+	return &Vertex{x, y}
 }
 
 func main() {
-	v := Vertex{3, 4}
-	fmt.Println(Area(v))
+	//v := Vertex{3, 4}
+	//fmt.Println(Area(v))
+	v := New(3, 4)
 	v.Scale(10)
 	fmt.Println(v.Area())
-
 }
