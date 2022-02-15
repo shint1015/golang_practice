0983ed5ed3bdd4e0b8c1e820eae8a868b5f17e86
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-15 23:27:27 +0900

    Embebbed practice

--- practice.go
+++ practice.go
@@ -6,32 +6,38 @@ type Vertex struct {
 	x, y int
 }
 
-func Area(v Vertex) int {
-	return v.x * v.y
-}
-
-// 値レシーバー
 func (v Vertex) Area() int {
 	return v.x * v.y
 }
 
-// ポインタレシーバー
-//　参照して、structの中身に変更を加える
 func (v *Vertex) Scale(i int) {
 	v.x = v.x * i
 	v.y = v.y * i
 }
 
-//Golangのパターン
-//newでストラクタを作成することがよくあるパターン
-func New(x, y int) *Vertex {
-	return &Vertex{x, y}
+type Vertex3D struct {
+	Vertex //vertexを渡せる
+	z      int
+}
+
+func (v Vertex3D) Area3D() int {
+	return v.x * v.y * v.z
+}
+
+func (v *Vertex3D) Scale3D(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+	v.z = v.z * i
+}
+
+func New(x, y, z int) *Vertex3D {
+	//Vertex3Dの中で、Vertexを宣言することができる
+	return &Vertex3D{Vertex{x, y}, z}
 }
 
 func main() {
-	//v := Vertex{3, 4}
-	//fmt.Println(Area(v))
-	v := New(3, 4)
-	v.Scale(10)
+	v := New(3, 4, 5)
+	v.Scale3D(10)
 	fmt.Println(v.Area())
+	fmt.Println(v.Area3D())
 }
