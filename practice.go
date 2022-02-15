1144ee409cfe654ce4c4f8eeb04760410752bcd6
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-15 23:52:39 +0900

    interface ducktyping practice

--- practice.go
+++ practice.go
@@ -2,16 +2,42 @@ package main
 
 import "fmt"
 
-type MyInt int
+type Human interface {
+	Say() string
+}
 
-func (i MyInt) Double() int {
-	fmt.Printf("%T %v\n", i, i)
-	fmt.Printf("%T %v\n", 1, 1)
-	//このiはintではないので、castしてあげないといけない
-	return int(i * 2)
+type Person struct {
+	Name string
+}
+
+type Dog struct {
+	Name string
+}
+
+func (p *Person) Say() string {
+	//中身に変更を加えたい時は、アドレスを渡す必要がある。
+	p.Name = "Mr." + p.Name
+	return p.Name
+}
+
+func DriveCar(human Human) {
+	if human.Say() == "Mr.Mike" {
+		fmt.Println("run")
+	} else {
+		fmt.Println("get out")
+	}
 }
 
 func main() {
-	myInt := MyInt(10)
-	fmt.Println(myInt.Double())
+	//Humanというinterfaceの中に、Personを入れる
+	var mike Human = &Person{"Mike"}
+	var x Human = &Person{"x"}
+	//別のストラクトだとエラーが起きる
+	var dog Dog = Dog{"Tom"}
+
+	DriveCar(mike)
+	DriveCar(x)
+	fmt.Println(dog)
+	//DriveCar(dog)
+
 }
