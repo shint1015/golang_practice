55497996118a2b05cfa815246243733a48121a48
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-15 23:37:58 +0900

    non-struct practice

--- practice.go
+++ practice.go
@@ -2,42 +2,16 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	x, y int
-}
+type MyInt int
 
-func (v Vertex) Area() int {
-	return v.x * v.y
-}
-
-func (v *Vertex) Scale(i int) {
-	v.x = v.x * i
-	v.y = v.y * i
-}
-
-type Vertex3D struct {
-	Vertex //vertexを渡せる
-	z      int
-}
-
-func (v Vertex3D) Area3D() int {
-	return v.x * v.y * v.z
-}
-
-func (v *Vertex3D) Scale3D(i int) {
-	v.x = v.x * i
-	v.y = v.y * i
-	v.z = v.z * i
-}
-
-func New(x, y, z int) *Vertex3D {
-	//Vertex3Dの中で、Vertexを宣言することができる
-	return &Vertex3D{Vertex{x, y}, z}
+func (i MyInt) Double() int {
+	fmt.Printf("%T %v\n", i, i)
+	fmt.Printf("%T %v\n", 1, 1)
+	//このiはintではないので、castしてあげないといけない
+	return int(i * 2)
 }
 
 func main() {
-	v := New(3, 4, 5)
-	v.Scale3D(10)
-	fmt.Println(v.Area())
-	fmt.Println(v.Area3D())
+	myInt := MyInt(10)
+	fmt.Println(myInt.Double())
 }
