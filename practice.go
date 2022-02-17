f052ffb5a0305e258a2b6b4751d129bf36dd10c4
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-17 23:15:55 +0900

    chanel 1 practice

--- practice.go
+++ practice.go
@@ -1,35 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
 //goroutine 並列処理
+//chanel goroutineと通常処理のデータの受け渡しで使う
 
-func goroutine(s string, wg *sync.WaitGroup) {
-	for i := 0; i < 5; i++ {
-		//time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
+func goroutine1(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
 	}
-	wg.Done()
-}
-
-func normal(s string) {
-	for i := 0; i < 5; i++ {
-		//time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
-	}
+	//chanelに値を渡す矢印
+	c <- sum
 }
 
 func main() {
-	//処理が終了するのを待つための処理
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go goroutine("world", &wg)
-	//goroutineのコマンドが終了しなくても、
-	//通常の処理が終わってしまったら、処理が終了する
-	normal("hello")
-	//time.Sleep(2000 * time.Millisecond)
-	wg.Wait()
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int)
+	go goroutine1(s, c)
+	x := <-c
+	fmt.Println(x)
 }
