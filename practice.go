cf30ab0dfdbe56bbe018ec12a6fd7e2a5acdc62a
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-17 23:36:09 +0900

    buffered chanel practice

--- practice.go
+++ practice.go
@@ -5,32 +5,26 @@ import "fmt"
 //goroutine 並列処理
 //chanel goroutineと通常処理のデータの受け渡しで使う
 
-func goroutine1(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	//chanelに値を渡す矢印
-	c <- sum
-}
-
-func goroutine2(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	//chanelに値を渡す矢印
-	c <- sum
-}
-
 func main() {
-	s := []int{1, 2, 3, 4, 5}
-	c := make(chan int)
-	go goroutine1(s, c)
-	go goroutine2(s, c)
-	x := <-c
-	fmt.Println(x)
-	y := <-c
-	fmt.Println(y)
+	//第二引数が、最大のチャネルの個数
+	ch := make(chan int, 2)
+	ch <- 100
+	fmt.Println(len(ch))
+	ch <- 200
+	fmt.Println(len(ch))
+
+	//チャネルから値を取得している
+	//x := <-ch
+	//fmt.Println(x)
+	//y := <-ch
+	//fmt.Println(y)
+	//この時点で、2個取ったので、チャネルが空っぽになっている
+	//だから、再度チャネルに入れることができる
+
+	//rangeでチャネルを回す時は、closeでチャネルを終了しないといけない
+	close(ch)
+	for c := range ch {
+		fmt.Println(c)
+	}
 
 }
