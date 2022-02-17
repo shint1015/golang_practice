b317bb30f80cf801a528d543bc6fb3e419634440
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-17 23:06:36 +0900

    goroutine practice

--- practice.go
+++ practice.go
@@ -1,24 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-//Q1. 以下に、7と表示されるメソッドを作成してください。
-//Q2. X is 3! Y is 4! と表示されるStringerを作成してください。
+//goroutine 並列処理
 
-type Vertex struct {
-	X, Y int
+func goroutine(s string, wg *sync.WaitGroup) {
+	for i := 0; i < 5; i++ {
+		//time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+	wg.Done()
 }
 
-func (v Vertex) Plus() int {
-	return v.X + v.Y
-}
-
-func (v Vertex) String() string {
-	return fmt.Sprintf("X is %v! Y is %v!", v.X, v.Y)
+func normal(s string) {
+	for i := 0; i < 5; i++ {
+		//time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
 }
 
 func main() {
-	v := Vertex{3, 4}
-	fmt.Println(v.Plus())
-	fmt.Println(v)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go goroutine("world", &wg)
+	//goroutineのコマンドが終了しなくても、
+	//通常の処理が終わってしまったら、処理が終了する
+	normal("hello")
+	//time.Sleep(2000 * time.Millisecond)
+	wg.Wait()
 }
