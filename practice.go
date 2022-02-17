47242599365721b196335fcd4fa9b817a79396a2
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-17 23:19:34 +0900

    chanel 2 practice

--- practice.go
+++ practice.go
@@ -14,10 +14,23 @@ func goroutine1(s []int, c chan int) {
 	c <- sum
 }
 
+func goroutine2(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	//chanelに値を渡す矢印
+	c <- sum
+}
+
 func main() {
 	s := []int{1, 2, 3, 4, 5}
 	c := make(chan int)
 	go goroutine1(s, c)
+	go goroutine2(s, c)
 	x := <-c
 	fmt.Println(x)
+	y := <-c
+	fmt.Println(y)
+
 }
