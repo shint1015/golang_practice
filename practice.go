d36b5935f3c2a7d079fff35a9aad73d334abb08d
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-19 00:12:20 +0900

    channel range close practice

--- practice.go
+++ practice.go
@@ -3,28 +3,23 @@ package main
 import "fmt"
 
 //goroutine 並列処理
-//chanel goroutineと通常処理のデータの受け渡しで使う
+
+func goroutine1(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+		c <- sum
+	}
+	//chanは、処理が終了したら、closeする
+	//closeしないと、rangeで回した時にエラーが起きる
+	close(c)
+}
 
 func main() {
-	//第二引数が、最大のチャネルの個数
-	ch := make(chan int, 2)
-	ch <- 100
-	fmt.Println(len(ch))
-	ch <- 200
-	fmt.Println(len(ch))
-
-	//チャネルから値を取得している
-	//x := <-ch
-	//fmt.Println(x)
-	//y := <-ch
-	//fmt.Println(y)
-	//この時点で、2個取ったので、チャネルが空っぽになっている
-	//だから、再度チャネルに入れることができる
-
-	//rangeでチャネルを回す時は、closeでチャネルを終了しないといけない
-	close(ch)
-	for c := range ch {
-		fmt.Println(c)
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int, len(s))
+	go goroutine1(s, c)
+	for i := range c {
+		fmt.Println(i)
 	}
-
 }
