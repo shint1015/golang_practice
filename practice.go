71fc5763ba5c4feafd4a6b2c04c3442de326c2ff
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-20 18:26:24 +0900

    prepare file

--- practice.go
+++ practice.go
@@ -1,30 +1,31 @@
 package main
 
-import "fmt"
-
-//goroutine 並列処理
-//Q1:間違えを探せ
-
-//修正前
-//func goroutine1(arr []string, ch chan int) {
-//修正後
-func goroutine1(arr []string, ch chan string) {
-	sum := ""
-	for _, v := range arr {
-		sum += v
-		ch <- sum
-	}
-	close(ch)
-}
-
-func main() {
-	arr := []string{"test1!", "test2!", "test3!", "test4!"}
-	//修正前
-	//ch := make(chan int)
-	//修正後
-	ch := make(chan string)
-	go goroutine1(arr, ch)
-	for v := range ch {
-		fmt.Println(v)
-	}
-}
+//
+//import "fmt"
+//
+////goroutine 並列処理
+////Q1:間違えを探せ
+//
+////修正前
+////func goroutine1(arr []string, ch chan int) {
+////修正後
+//func goroutine1(arr []string, ch chan string) {
+//	sum := ""
+//	for _, v := range arr {
+//		sum += v
+//		ch <- sum
+//	}
+//	close(ch)
+//}
+//
+//func main() {
+//	arr := []string{"test1!", "test2!", "test3!", "test4!"}
+//	//修正前
+//	//ch := make(chan int)
+//	//修正後
+//	ch := make(chan string)
+//	go goroutine1(arr, ch)
+//	for v := range ch {
+//		fmt.Println(v)
+//	}
+//}
