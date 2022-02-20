36f4000456bae992150ee3196538981ace4a89d8
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-20 17:28:09 +0900

    sync mutex practice

--- practice.go
+++ practice.go
@@ -2,32 +2,46 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 //goroutine 並列処理
+//同じ物に並列処理で変更を加えた際にエラーが起きることがある
+//対応として、muxを使う
+
+type Counter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+func (c *Counter) Inc(key string) {
+	//変更を加える際は、対象への変更をロックできる
+	c.mux.Lock()
+	//変更を加えた後は、ロックを解除
+	defer c.mux.Unlock()
+	c.v[key]++
+}
+
+func (c *Counter) Value(key string) int {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
+}
 
 func main() {
-	tick := time.Tick(100 * time.Millisecond)
-	boom := time.After(500 * time.Millisecond)
-	//for breakを使う場合は,変数名：でスタートし
-	//break 変数名でやればbreakすることができる
-OuterBreak:
-	for {
-		select {
-		case <-tick:
-			fmt.Println("tick.")
-		case <-boom:
-			fmt.Println("boom!")
-			//処理自体終了してします
-			return
-			//for文を終わらせるためには、
-			break OuterBreak
-		default:
-			fmt.Println("    .")
-			time.Sleep(50 * time.Millisecond)
-
+	//c := make(map[string]int)
+	c := Counter{v: make(map[string]int)}
+	go func() {
+		for i := 0; i < 10; i++ {
+			c.Inc("key")
 		}
-	}
-	fmt.Println("####################")
+	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			c.Inc("key")
+		}
+	}()
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println(c, c.Value("key"))
 }
