f5dc2d94627849668207edf996f46c899792e975
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-20 16:39:51 +0900

    default selection for break practice

--- practice.go
+++ practice.go
@@ -7,39 +7,27 @@ import (
 
 //goroutine 並列処理
 
-func goroutine1(ch chan string) {
-	for {
-		ch <- "package from 1"
-		time.Sleep(2 * time.Second)
-	}
-}
-
-func goroutine2(ch chan int) {
-	for {
-		ch <- 200
-		time.Sleep(2 * time.Second)
-	}
-}
-
 func main() {
-	//channelを分けて、routineを分けることができる
-	c1 := make(chan string)
-	c2 := make(chan int)
-	go goroutine1(c1)
-	go goroutine2(c2)
-
-
-	//selectを使って分ける
-	//書き方は以下のようなやり方でやる
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	//for breakを使う場合は,変数名：でスタートし
+	//break 変数名でやればbreakすることができる
+OuterBreak:
 	for {
 		select {
-		//<-c1の値が入ってきたら
-		case msg1 := <-c1:
-			fmt.Println(msg1)
-		//<-c2の値が入ってきたら
-		case msg2 := <-c2:
-			fmt.Println(msg2)
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("boom!")
+			//処理自体終了してします
+			return
+			//for文を終わらせるためには、
+			break OuterBreak
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
 
 		}
 	}
+	fmt.Println("####################")
 }
