f6c3761a7b24abe4fc97e700b8c67dbecc3be72f
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-20 17:47:11 +0900

    ennsyuu practice

--- practice.go
+++ practice.go
@@ -1,47 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
 //goroutine 並列処理
-//同じ物に並列処理で変更を加えた際にエラーが起きることがある
-//対応として、muxを使う
+//Q1:間違えを探せ
 
-type Counter struct {
-	v   map[string]int
-	mux sync.Mutex
-}
-
-func (c *Counter) Inc(key string) {
-	//変更を加える際は、対象への変更をロックできる
-	c.mux.Lock()
-	//変更を加えた後は、ロックを解除
-	defer c.mux.Unlock()
-	c.v[key]++
-}
-
-func (c *Counter) Value(key string) int {
-	c.mux.Lock()
-	defer c.mux.Unlock()
-	return c.v[key]
+//修正前
+//func goroutine1(arr []string, ch chan int) {
+//修正後
+func goroutine1(arr []string, ch chan string) {
+	sum := ""
+	for _, v := range arr {
+		sum += v
+		ch <- sum
+	}
+	close(ch)
 }
 
 func main() {
-	//c := make(map[string]int)
-	c := Counter{v: make(map[string]int)}
-	go func() {
-		for i := 0; i < 10; i++ {
-			c.Inc("key")
-		}
-	}()
-	go func() {
-		for i := 0; i < 10; i++ {
-			c.Inc("key")
-		}
-	}()
-	time.Sleep(500 * time.Millisecond)
-	fmt.Println(c, c.Value("key"))
+	arr := []string{"test1!", "test2!", "test3!", "test4!"}
+	//修正前
+	//ch := make(chan int)
+	//修正後
+	ch := make(chan string)
+	go goroutine1(arr, ch)
+	for v := range ch {
+		fmt.Println(v)
+	}
 }
