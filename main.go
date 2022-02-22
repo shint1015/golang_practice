6cdcd1165ddc5e416d245433e7e1c41d66094c92
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-22 23:51:09 +0900

    context practice

--- main.go
+++ main.go
@@ -1,25 +1,44 @@
 package main
 
-import "fmt"
-
-//iota
-//自動採番
-
-const (
-	c1 = iota
-	c2
-	c3
+import (
+	"context"
+	"fmt"
+	"time"
 )
 
-//１を10回シフトする
-const (
-	_      = iota
-	KB int = 1 << (10 * iota) //10
-	MB                        //20
-	GB                        //30
-)
+//context
+//処理に時間がかかった時に、キャンセルできる
+
+func longProcess(ctx context.Context, ch chan string) {
+	fmt.Println("run")
+	time.Sleep(2 * time.Second)
+	fmt.Println("finish")
+	ch <- "result"
+}
 
 func main() {
-	fmt.Println(c1, c2, c3) //0,1,2
-	fmt.Println(KB, MB, GB)
+	ch := make(chan string)
+	//contextの呼び出し
+	ctx := context.Background()
+	//context 終了条件の設定
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+	go longProcess(ctx, ch)
+
+	//goroutineの終了する
+	//cancel()
+
+CTXLOOP:
+	for {
+		select {
+		case <-ctx.Done()://ctxの終了に引っかかった時
+			fmt.Println(ctx.Err())//errorの文言を表示
+			break CTXLOOP
+		case <-ch:
+			fmt.Println("success")
+			break CTXLOOP
+		}
+	}
+	fmt.Println("##################")
+
 }
