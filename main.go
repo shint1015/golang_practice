4103fc8acada0a906b08dc42221da0c576ccb954
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-22 23:20:03 +0900

    iota practice

--- main.go
+++ main.go
@@ -1,33 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"sort"
+import "fmt"
+
+//iota
+//自動採番
+
+const (
+	c1 = iota
+	c2
+	c3
 )
 
-//sort
+//１を10回シフトする
+const (
+	_      = iota
+	KB int = 1 << (10 * iota) //10
+	MB                        //20
+	GB                        //30
+)
 
 func main() {
-	i := []int{2, 1, 5, 9, 7, 3}
-	s := []string{"A", "S", "D"}
-	p := []struct {
-		Name string
-		Age  int
-	}{
-		{"Nancy", 20},
-		{"Vera", 40},
-		{"Mike", 30},
-		{"Bob", 50},
-	}
-	fmt.Println(i, s, p)
-	//整数のソート
-	sort.Ints(i)
-	//文字列のソート
-	sort.Strings(s)
-	//struct?連想配列?の文字列ソート
-	sort.Slice(p, func(i, j int) bool { return p[i].Name < p[j].Name })
-	//struct?連想配列?の数値ソート
-	sort.Slice(p, func(i, j int) bool { return p[i].Age < p[j].Age })
-
-	fmt.Println(i, s, p)
+	fmt.Println(c1, c2, c3) //0,1,2
+	fmt.Println(KB, MB, GB)
 }
