6cb058495f495c95731ddc18b97e817ca8883082
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-22 22:56:02 +0900

    regexp practice

--- main.go
+++ main.go
@@ -1,28 +1,40 @@
 package main
 
-//func main() {
-//	s := []int{1, 2, 3, 4, 5}
-//	fmt.Println(mylib.Average(s))
-//
-//	mylib.Say()
-//	person := mylib.Person{Name: "taro", Age: 21}
-//	fmt.Println(person)
-//
-//	fmt.Println(mylib.Public)
-//
-//	//fmt.Println(mylib.private)
-//
-//	under.Hello()
-//}
-
 import (
 	"fmt"
-	"time"
+	"regexp"
 )
 
+//regexp
+//正規表現
+
 func main() {
-	t := time.Now()
-	fmt.Println(t)
-	fmt.Println(t.Format(time.RFC3339))
-	fmt.Println(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+	match, _ := regexp.MatchString("a([a-z0-9]+)e", "appl0e")
+	fmt.Println(match)
+
+	//MustCompile
+	//正規表現の条件を使い回す時
+	r := regexp.MustCompile("a([a-z]+)e")
+	ms := r.MatchString("apple")
+	fmt.Println(ms)
+
+	//s := "view/test"
+	r2 := regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+	//FindString
+	//一致するときに、変数に値が入る
+	fs := r2.FindString("/view/testaaa")
+	fmt.Println(fs)
+	fs2 := r2.FindString("/viewwww/test")
+	fmt.Println(fs2)
+
+	//FindStringSubmatch
+	//条件と一致する時、スライスで値が帰ってくる
+	fss := r2.FindStringSubmatch("/view/test")
+	fmt.Println(fss, fss[0], fss[1], fss[2])
+	fss = r2.FindStringSubmatch("/edit/test")
+	fmt.Println(fss, fss[0], fss[1], fss[2])
+	fss = r2.FindStringSubmatch("/save/test")
+	fmt.Println(fss, fss[0], fss[1], fss[2])
+
 }
