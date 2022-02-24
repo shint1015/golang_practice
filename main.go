aff9a7b1c6008bc01440236d1dd3c4d50340385e
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-24 23:28:31 +0900

    HMAC practice

--- main.go
+++ main.go
@@ -1,70 +1,43 @@
 package main
 
 import (
-	"encoding/json"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 )
 
-type T struct{}
+//hmac
+//APIでサーバーへアクセスする際に
+//認証情報をヘッダーに含めることが多いのでそれで使う
 
-//以下のような形で、渡すとキー名を変換できる
-//`json:"name"`
-//型を指定して渡す場合
-//`json:"name, string(型名)"`
-//jsonにする際に、非表示にする場合
-//`json:"-"`
-//空の場合、値を渡さない場合
-//`json:"name, omitempty"`
-//structにomitemptyを使う場合
-//T *T（ポインタで渡す必要がある） `json:"T,omitempty"`
-
-type Person struct {
-	Name      string   `json:"name,omitempty"`
-	Age       int      `json:"age"`
-	Nicknames []string `json:"nicknames"`
-	T         *T       `json:"T,omitempty"`
+var DB = map[string]string{
+	"User1Key": "User1Secret",
+	"User2Key": "User2Secret",
 }
 
-//UnMarshalをカスタマイズする
-//func (p *Person) UnmarshalJSON(b []byte) error {
-//	type Person2 struct {
-//		Name string
-//		Age  int
-//	}
-//	var p2 Person2
-//	err := json.Unmarshal(b, &p2)
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//	p.Name = p2.Name + "!"
-//	p.Age = p2.Age * 10
-//	return err
-//}
-
-//Marshalをカスタマイズする
-//func (p Person) MarshalJSON() ([]byte, error) {
-//	v, err := json.Marshal(&struct {
-//		Name string
-//	}{
-//		Name: "Mr." + p.Name,
-//	})
-//
-//	return v, err
-//}
+func Server(apiKey, sign string, data []byte) {
+	apiSecret := DB[apiKey]
+	h := hmac.New(sha256.New, []byte(apiSecret))
+	h.Write(data)
+	expectedHMAC := hex.EncodeToString(h.Sum(nil))
+	fmt.Println(sign == expectedHMAC)
+}
 
 func main() {
-	b := []byte(`{"name":"Tom","age":20, "nicknames":["a","b","c"]}`)
-	var p Person
-	//Unmarshal
-	//ネットワークを入ってきたものを、そのままstructに入れてくれる
-	//小文字でも大文字でも判定して、入れてくれる
-	if err := json.Unmarshal(b, &p); err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(p.Name, p.Age, p.Nicknames)
-	//Marshal
-	//jsonに変換する関数
-	v, _ := json.Marshal(p)
-	fmt.Println(string(v))
+	const apiKey = "User1Key"
+	const apiSecret = "User1Secret"
+
+	//■■■■■■■■■■■■■■■■■■　基本の形　■■■■■■■■■■■■■■■■■■■//
+	data := []byte("data")
+	h := hmac.New(sha256.New, []byte(apiSecret))
+	h.Write(data)
+	sign := hex.EncodeToString(h.Sum(nil))
+	//■■■■■■■■■■■■■■■■■■　基本の形　■■■■■■■■■■■■■■■■■■■//
+
+	//fmt.Println(sign)
+
+	//server側の処理
+	Server(apiKey, sign, data)
 
 }
