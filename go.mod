1aafd2a4560dd2f7723a83d42e2bd4145fc8df55
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-02-26 23:02:58 +0900

    ini practice

--- go.mod
+++ go.mod
@@ -12,4 +12,5 @@ require (
 	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 	golang.org/x/tools v0.1.9 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
 )
--- main.go
+++ main.go
@@ -1,46 +1,44 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
 
-	"golang.org/x/sync/semaphore"
+	"gopkg.in/ini.v1"
 )
 
-//semaphore
-//goroutineの同時実行数を制限できる
-var s *semaphore.Weighted = semaphore.NewWeighted(1)
+//ini
+//configファイルから呼び出すことができる
+//例 config.ini
+// [web]
+//	port=8080
 
-func longProcess(ctx context.Context) {
+//取得方法
+//cfg, _ := ini.Load("config.ini")
+//cfg.Section("web").Key("port").MustInt()
 
-	//goroutineの制限数を超えた場合、goroutineを終了させる時
-	isAcquire := s.TryAcquire(1)
-	if !isAcquire {
-		fmt.Println("Could not get lock")
-		return
+//MustString(default値)でカッコ内に値を入れることで、configファイルの値が空もしくはない場合にデフォルト値を参照する
+//pkg詳細
+//https://pkg.go.dev/gopkg.in/ini.v1
+
+type ConfigList struct {
+	Port      int
+	DbName    string
+	SQLDriver string
+}
+
+var Config ConfigList
+
+func init() {
+	cfg, _ := ini.Load("config.ini")
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("port").MustInt(),
+		DbName:    cfg.Section("db").Key("name").MustString("example.sql"),
+		SQLDriver: cfg.Section("db").Key("driver").MustString(),
 	}
-
-	//goroutineの制限数を超えた場合、goroutineを待機させる時
-	//s.Acquire(ctx, 1)でロックする
-	//if err := s.Acquire(ctx, 1); err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-	defer s.Release(1)
-	fmt.Println("Wait...")
-	time.Sleep(1 * time.Second)
-	fmt.Println("Done")
 }
 
 func main() {
-	ctx := context.TODO()
-	go longProcess(ctx)
-	go longProcess(ctx)
-	go longProcess(ctx)
-	time.Sleep(2 * time.Second)
-	go longProcess(ctx)
-	time.Sleep(5 * time.Second)
-
-	fmt.Println("Finish!!")
+	fmt.Printf("%T %v \n", Config.Port, Config.Port)
+	fmt.Printf("%T %v \n", Config.DbName, Config.DbName)
+	fmt.Printf("%T %v \n", Config.SQLDriver, Config.SQLDriver)
 }
