a706de3c32bd90224629ac839e589223e7e61d45
shint1015 <53039633+shint1015@users.noreply.github.com>
2022-03-08 14:00:34 +0900

    webApp5 practice

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -30,16 +29,16 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-var templates = template.Must(template.ParseFiles("edit.html, view.html"))
-
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	//t, _ := template.ParseFiles(tmpl + ".html")
+	//t.Execute(w, p)
+
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-	//t, _ := template.ParseFiles(tmpl + ".html")
-	//t.Execute(w, p)
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -75,33 +74,20 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
-
-func  makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request){
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil{
-			http.NotFound(w,r)
+		if m == nil {
+			http.NotFound(w, r)
 			return
 		}
-		fn(w,r,m[2])
+		fn(w, r, m[2])
 	}
 }
 
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	title := "miyata"
-	p, _ := loadPage(title)
-
-	//少量のhtmlを出力する時
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
-
-	//templateHTMLへ書き加える時
-	//renderTemplate(w, "view", p)
-}
-
 func main() {
-	http.HandleFunc("/miyata/", testHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
-	log.Fatalln(http.ListenAndServe(":6060", nil))
+	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
